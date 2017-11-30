7bb9a288db406a7427d5d979e9e0128719e40c45
Johannes Pohl <Johannes.Pohl90@gmail.com>
2017-11-30 16:54:07 +0100

    fix windows stdout stderr

--- src/urh/main.py
+++ src/urh/main.py
@@ -11,28 +11,6 @@ from PyQt5.QtGui import QPalette, QIcon, QColor
 from PyQt5.QtWidgets import QApplication, QWidget, QStyleFactory
 
 
-# Processes can't write to stdout/stderr on frozen windows apps because they do not exist here
-# if process tries it anyway we get a nasty dialog window popping up, so we redirect the streams to a dummy
-if hasattr(sys, "frozen") and sys.platform == "win32":
-    try:
-        sys.stdout.write("\n")
-        sys.stdout.flush()
-    except:
-        class DummyStream(object):
-            def __init__(self): pass
-            def write(self, data): pass
-            def read(self, data): pass
-            def flush(self): pass
-            def close(self): pass
-
-        sys.stdout = DummyStream()
-        sys.stderr = DummyStream()
-        sys.stdin = DummyStream()
-        sys.__stdout__ = DummyStream()
-        sys.__stderr__ = DummyStream()
-        sys.__stdin__ = DummyStream()
-
-
 try:
     locale.setlocale(locale.LC_ALL, '')
 except locale.Error as e:
@@ -41,9 +19,36 @@ except locale.Error as e:
 GENERATE_UI = True
 
 
+def fix_windows_stdout_stderr():
+    """
+    Processes can't write to stdout/stderr on frozen windows apps because they do not exist here
+    if process tries it anyway we get a nasty dialog window popping up, so we redirect the streams to a dummy
+    see https://github.com/jopohl/urh/issues/370
+    """
+
+    if hasattr(sys, "frozen") and sys.platform == "win32":
+        try:
+            sys.stdout.write("\n")
+            sys.stdout.flush()
+        except:
+            class DummyStream(object):
+                def __init__(self): pass
+
+                def write(self, data): pass
+
+                def read(self, data): pass
+
+                def flush(self): pass
+
+                def close(self): pass
+
+            sys.stdout, sys.stderr, sys.stdin = DummyStream(), DummyStream(), DummyStream()
+            sys.__stdout__, sys.__stderr__, sys.__stdin__ = DummyStream(), DummyStream(), DummyStream()
 
 
 def main():
+    fix_windows_stdout_stderr()
+
     if sys.version_info < (3, 4):
         print("You need at least Python 3.4 for this application!")
         sys.exit(1)
